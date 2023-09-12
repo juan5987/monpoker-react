8a22727b7048e5861a2b6d29f28b1eca77d2a5ff
Juan5987 <juan.machado.5987@gmail.com>
2023-09-12 13:44:53 +0200

    working on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,17 @@ import '../styles/header.sass';
 
 const Header = () => {
   return (
-    <div className='header'>Header</div>
+    <div className='header'>
+      <div className="header__logo">
+        <img className='header__logo__img' src="" alt="logo" />
+        <span className='header__logo__text'>monpoker</span>
+      </div>
+      <div className="header__burger">
+        <span className="header__burger__line"></span>
+        <span className="header__burger__line"></span>
+        <span className="header__burger__line"></span>
+      </div>
+    </div>
   )
 }
 
