6f2e53816cc828de6f900b246cd1f2518982c583
Juan5987 <juan.machado.5987@gmail.com>
2023-09-13 11:44:30 +0200

    working on header and navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/header.sass';
 
 const Header = () => {
+
+  const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
+
   return (
-    <div className='header'>
-      <div className="header__logo">
-        <img className='header__logo__img' src="" alt="logo" />
-        <span className='header__logo__text'>monpoker</span>
+    <>
+      <div className='header'>
+        <div className="header__logo">
+          <span className='header__logo__text'>monpoker</span>
+        </div>
+        <div className="header__burger" onClick={() => setIsMenuOpened(!isMenuOpened)}>
+          <span className="header__burger__line"></span>
+          <span className="header__burger__line"></span>
+          <span className="header__burger__line"></span>
+        </div>
       </div>
-      <div className="header__burger">
-        <span className="header__burger__line"></span>
-        <span className="header__burger__line"></span>
-        <span className="header__burger__line"></span>
-      </div>
-    </div>
+      <nav className={isMenuOpened ? "navbar" : "navbar--open"}>
+        <ul className="navbar__list">
+          <li className="navbar__list__element">
+            <a href="/" className="navbar__list__element__link">accueil</a>
+          </li>
+          <li className="navbar__list__element">
+            <a href="/meals" className="navbar__list__element__link">repas</a>
+          </li>
+          <li className="navbar__list__element">
+            <a href="/planning" className="navbar__list__element__link">planning</a>
+          </li>
+          <li className="navbar__list__element">
+            <a href="/account" className="navbar__list__element__link">compte</a>
+          </li>
+          <li className="navbar__list__element">
+            <a href="/logout" className="navbar__list__element__link">déconnexion</a>
+          </li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
