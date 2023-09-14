5ddc2b9c19913c8887311b701f70bc8e217ef098
Juan5987 <juan.machado.5987@gmail.com>
2023-09-14 15:04:16 +0200

    added form submit logic whit hooks for api, service and type

--- src/pages/CreateTournament.tsx
+++ src/pages/CreateTournament.tsx
@@ -1,38 +1,57 @@
 import React from 'react'
+import { useForm } from "react-hook-form"
+import { createTournament } from '../services/apiCreateTournament'
+import { ITournament } from '../types/tournament'
 import '../styles/create-tournament.sass'
 import '../styles/create-tournament-desktop.sass'
 
 const CreateTournament = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const onSubmit = async (data: any) => {
+        try {
+            const response = await createTournament(data);
+            console.log(response)
+        } catch (error:any) {
+            console.log(error.message)
+        }
+    }
+
     return (
         <section className='createtournament'>
             <div className="createtournament__title">
                 <h2 className='createtournament__title__text'>Créer un tournoi</h2>
             </div>
             <span className='createtournament__form__info'>Tous les champs doivent être complétés</span>
-            <form className="createtournament__form">
+            <form className="createtournament__form" onSubmit={handleSubmit(onSubmit)}>
                 <div className="createtournament__form__element">
                     <label htmlFor="name" className="createtournament__form__element__label">Nom du tournoi</label>
-                    <input type="text" className="createtournament__form__element__input" id="name" name="name" />
+                    <input type="text" className="createtournament__form__element__input" id="name" {...register("name", { required: true })} />
+                    {errors.name?.type === 'required' && <p role="alert" className='createtournament__form__error'>Veuillez renseigner le nom du tournoi</p>}
                 </div>
                 <div className="createtournament__form__element">
                     <label htmlFor="date" className="createtournament__form__element__label">Date du tournoi</label>
-                    <input type="date" className="createtournament__form__element__input" id="date" name="date" />
+                    <input type="date" className="createtournament__form__element__input" id="date" {...register("date", { required: true })} />
+                    {errors.date?.type === 'required' && <p role="alert" className='createtournament__form__error'>Veuillez renseigner la date</p>}
                 </div>
                 <div className="createtournament__form__element">
                     <label htmlFor="players" className="createtournament__form__element__label">Nombre de joueurs</label>
-                    <input type="number" className="createtournament__form__element__input" id="players" name="players" step={1} />
+                    <input type="number" className="createtournament__form__element__input" id="players" step={1} {...register("players", { required: true })} />
+                    {errors.players?.type === 'required' && <p role="alert" className='createtournament__form__error'>Veuillez renseigner le nombre de joueurs</p>}
                 </div>
                 <div className="createtournament__form__element">
                     <label htmlFor="buyin" className="createtournament__form__element__label">Mise</label>
-                    <input type="number" className="createtournament__form__element__input" id="buyin" name="buyin" step={1} placeholder='€' />
+                    <input type="number" className="createtournament__form__element__input" id="buyin" step={1} placeholder='€' {...register("buyin", { required: true })} />
+                    {errors.buyin?.type === 'required' && <p role="alert" className='createtournament__form__error'>Veuillez renseigner la mise</p>}
                 </div>
                 <div className="createtournament__form__element">
                     <label htmlFor="prize" className="createtournament__form__element__label">Prix</label>
-                    <input type="number" className="createtournament__form__element__input" id="prize" name="prize" step={1} placeholder='€' />
+                    <input type="number" className="createtournament__form__element__input" id="prize" step={1} placeholder='€' {...register("prize", { required: true })} />
+                    {errors.prize?.type === 'required' && <p role="alert" className='createtournament__form__error'>Veuillez renseigner le prix</p>}
                 </div>
                 <div className="createtournament__form__element">
                     <label htmlFor="prize" className="createtournament__form__element__label">Statut</label>
-                    <select className="createtournament__form__element__input" id="prize" name="prize">
+                    <select className="createtournament__form__element__input" id="prize" {...register("status", { required: true })}>
                         <option className='createtournament__form__element__input__option' value="prévu">prévu</option>
                         <option className='createtournament__form__element__input__option' value="terminé">terminé</option>
                     </select>
---
+++ src/services/apiCreateTournament.ts
@@ -0,0 +1,24 @@
+import { ITournament } from '../types/tournament';
+
+const BASE_URL = 'http://localhost:3001';
+
+export async function createTournament(data:ITournament) {
+  try {
+    const response = await fetch(`${BASE_URL}/tournaments`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('Erreur lors de la création du tournoi');
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (error:any) {
+    throw new Error('Erreur lors de la création du tournoi');
+  }
+}
