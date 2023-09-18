833e8a8f16747446bb88210eaccc130b229b2cf9
Juan5987 <juan.machado.5987@gmail.com>
2023-09-18 10:31:32 +0200

    modified design and added checkbox to show/hide finished tournaments

--- src/pages/Tournaments/components/Tournament.tsx
+++ src/pages/Tournaments/components/Tournament.tsx
@@ -4,26 +4,9 @@ import '../../../styles/tournament.sass'
 
 const Tournament = ({status, nom, mise, prix, joueurs, date}:ITournament) => {
   return (
-    <div className={status === "pending" ? "tournament" : "tournament tournament--finished"}>
-      <h2 className={status === "pending" ? "tournament__title" : "tournament__title tournament__title--finished"}>{nom}</h2>
-      <div className={status === "pending" ? "tournament__info" : "tournament__info tournament__info--finished"}>
-        <div className="tournament__info__element">
-          <span className="tournament__info__element__label">mise</span>
-          <span className="tournament__info__element__text">{mise}€</span>
-        </div>
-        <div className="tournament__info__element">
-          <span className="tournament__info__element__label">prix</span>
-          <span className=" tournament__info__element__text">{prix}€</span>
-        </div>
-        <div className="tournament__info__element">
-          <span className="tournament__info__element__label">joueurs</span>
-          <span className=" tournament__info__element__text">{joueurs}</span>
-        </div>
-        <div className="tournament__info__element">
-          <span className="tournament__info__element__label">date</span>
-          <span className=" tournament__info__element__text">{date}</span>
-        </div>
-      </div>
+    <div className={status === "prévu" ? "tournament" : "tournament tournament--finished"}>
+      <h2 className={status === "prévu" ? "tournament__title" : "tournament__title tournament__title--finished"}>{nom}</h2>
+      <span className="tournament__date">26/09/2023</span>
     </div>
   )
 }
